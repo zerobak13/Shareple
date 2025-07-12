3c4ed46dfef4a4b92ba2e0e879e292676aa2f731
박제영 <bagjeyeong@bagjeyeong-ui-MacBookPro.local>
2025-07-12 17:00:04 +0900

    지역 선택추가

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -63,13 +63,41 @@ const MainPage = () => {
         <div>
             <h2>물품 검색</h2>
             <div style={{ marginBottom: '1rem' }}>
-                <input
-                    type="text"
+                {/* 지역 선택 필드 */}
+                <select
                     name="location"
-                    placeholder="지역"
                     value={filters.location}
                     onChange={handleChange}
-                />
+                >
+                    <option value="">직거래 장소 선택</option>
+                    {/* 서울시 25개 구 */}
+                    <option value="강남구">강남구</option>
+                    <option value="강동구">강동구</option>
+                    <option value="강북구">강북구</option>
+                    <option value="강서구">강서구</option>
+                    <option value="관악구">관악구</option>
+                    <option value="광진구">광진구</option>
+                    <option value="구로구">구로구</option>
+                    <option value="금천구">금천구</option>
+                    <option value="노원구">노원구</option>
+                    <option value="도봉구">도봉구</option>
+                    <option value="동대문구">동대문구</option>
+                    <option value="동작구">동작구</option>
+                    <option value="마포구">마포구</option>
+                    <option value="서대문구">서대문구</option>
+                    <option value="서초구">서초구</option>
+                    <option value="성동구">성동구</option>
+                    <option value="성북구">성북구</option>
+                    <option value="송파구">송파구</option>
+                    <option value="양천구">양천구</option>
+                    <option value="영등포구">영등포구</option>
+                    <option value="용산구">용산구</option>
+                    <option value="은평구">은평구</option>
+                    <option value="종로구">종로구</option>
+                    <option value="중구">중구</option>
+                    <option value="중랑구">중랑구</option>
+                </select>
+
                 <input
                     type="date"
                     name="deadline"
